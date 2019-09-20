3979f35ebd113e9718f3a13fd9bba838ab8bce56
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 16:52:06 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+	attr_accessor :waiter, :customer, :tip, :total
 
+	@@all = []
+
+	def initialize(waiter, customer, tip, total)
+	  @waiter = waiter
+	  @customer = customer
+	  @tip = tip
+	  @total = total
+	  @@all << self
+	end
+
+	def self.all
+	  @@all
+	end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,16 @@
 class Waiter
+	attr_accessor :name, :exp
+
+	@@all = []
+
+	def initialize(name, exp)
+	  @name = name
+	  @exp = exp
+	  @@all << self
+	end
+
+	def self.all
+	  @@all
+	end
 
 end
